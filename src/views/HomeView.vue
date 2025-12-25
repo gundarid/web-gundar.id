bfcb1ddec47263a112c72e8dc46a5ae4a5c63646
faizallmaullana <faizalmaulanasekias@gmail.com>
2025-12-26 01:00:43 +0700

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+var description = "gundar.id is an open-source platform designed to empower students by streamlining their academic life through efficient organization, collaboration, and automation."
+</script>
+
+<template>
+  <main>
+    <div> 
+      <section>
+        <div class="w-full h-screen flex flex-col justify-center text-lg gap-4 sm:p-24"><!-- Bagian sini bisa dibuat wrapper -->
+          <p class="text-6xl">gundar.id</p>
+          <p class="font-bold">The Open Source Academic Ecosystem.</p>
+          <p class="max-w-[1000px]">{{ description }}</p>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
